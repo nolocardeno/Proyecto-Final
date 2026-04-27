e45832c1ba39c855ac4102f32c03ce48ac85c70a
nolocardeno <manologorrion@hotmail.com>
2026-04-28 00:11:21 +0200

    Añadido icono para cuando no haya imagen en el documento

--- frontend/src/app/components/shared/document-preview-card/document-preview-card.ts
+++ frontend/src/app/components/shared/document-preview-card/document-preview-card.ts
@@ -1,7 +1,10 @@
 import { Component, input, output } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-document-preview-card',
+  imports: [FaIconComponent],
   templateUrl: './document-preview-card.html',
   styleUrl: './document-preview-card.scss',
 })
@@ -10,4 +13,6 @@ export class DocumentPreviewCardComponent {
   alt = input<string>('Vista previa del documento');
 
   openLightbox = output<string>();
+
+  protected readonly faImage = faImage;
 }
